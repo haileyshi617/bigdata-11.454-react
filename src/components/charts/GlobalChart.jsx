52ee74ea0569a9e2a7b882bafd2c813a4055b3dd
albatross97 <ruiwang704@gmail.com>
2021-11-16 20:52:36 -0500

    update

---
+++ src/components/charts/GlobalChart.jsx
@@ -0,0 +1,3 @@
+import * as d3 from 'd3'
+
+const dataSource = 'src/data/food-global.csv'
--- src/components/section/FoodGlobal.jsx
+++ src/components/section/FoodGlobal.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import * as d3 from "d3";
+import GlobalWrapper from '../charts/GlobalWrapper'
 
 const FoodGlobal = () => {
-  // const margin = {top: 10, right: 30, bottom: 30, left: 30},
-  //       width = 460 - margin.left - margin.right,
-  //       height = 500 - margin.top - margin.bottom;
-
-
-  // const svg = d3.select("#my_dataviz")
-  //   .append("svg")
-  //     .attr("width", width + margin.left + margin.right)
-  //     .attr("height", height + margin.top + margin.bottom)
-  //   .append("g")
-  //     .attr("transform", `translate(${margin.left}, ${margin.top})`);
-
-  d3.csv('../../data/food-global.csv').then(
-    function(data){
-      console.log(data)
-    }
-  )
-
   return (
     <div className="section-break">
       <h1><span className="red">Food security</span> of the Northern Triangle Region in the global context.</h1>
+      <GlobalWrapper/>
     </div>
   );
 }
