300435c0cc3d0a671cef16831dc40b6f97650fc3
HuiwenShi <huiwenshi@outlook.com>
2021-11-16 21:30:06 -0500

    d3 wrapper created

--- src/components/charts/GlobalWrapper.jsx
+++ src/components/charts/GlobalWrapper.jsx
@@ -1,19 +1,21 @@
 import React, { useRef, useState, useEffect } from 'react';
 import GlobalChart from './GlobalChart';
 
-const ChartWrapper = () => {
-    const chartArea = useRef(null);
-    const [chart, setChart] = useState(null);
+const ChartWrapper = ({ gender }) => {
+  const chartArea = useRef(null);
+  const [chart, setChart] = useState(null);
 
-    useEffect(() => {
-        if (!chart) {
-            setChart(new GlobalChart(chartArea.current));
-        } else {
-            chart.update();
-        }
-    }, [chart]);
+  useEffect(() => {
+    if (!chart) {
+      setChart(new GlobalChart(chartArea.current));
+    }
+    // skip the loading state, when data is still a pending promise
+    else if (chart.menData) {
+      chart.update(gender);
+    }
+  }, [chart, gender]);
 
-    return <div className="chart-area" ref={chartArea}></div>;
+  return <div className="chart-area" ref={chartArea}></div>;
 };
 
 export default ChartWrapper;
