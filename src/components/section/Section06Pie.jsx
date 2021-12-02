02cfddb53c4654dc48e15aa04077b01ec1831ac3
albatross97 <ruiwang704@gmail.com>
2021-12-02 14:46:12 -0500

    bubble

---
+++ src/components/section/Section06Pie.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardSection from '../ui/CardSection';
+
+const Section6Pie = () => {
+    return (
+        <div className="section">
+            <CardSection header={text.header} main={text.main} />
+        </div>
+    );
+};
+
+export default Section06Pie;
